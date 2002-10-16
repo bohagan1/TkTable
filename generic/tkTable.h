42ab7b38671f799be6dad4e5c9710db0fa9401fd
hobbs <hobbs>
2002-10-16 12:31:48 +0000

    * generic/tkTable.h: * generic/tkTableUtil.c: * generic/tkTableWin.c: more CONST84 casting fixes.

--- generic/tkTable.h
+++ generic/tkTable.h
@@ -9,7 +9,7 @@
  * See the file "license.txt" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkTable.h,v 1.12 2002/03/21 02:18:27 hobbs Exp $
+ * RCS: @(#) $Id: tkTable.h,v 1.13 2002/09/26 01:47:20 hobbs Exp $
  */
 
 #ifndef _TKTABLE_H_
@@ -521,20 +521,19 @@ extern int	Table_TagCmd _ANSI_ARGS_((ClientData clientData,
 extern void	Table_ClearHashTable _ANSI_ARGS_((Tcl_HashTable *hashTblPtr));
 extern int	TableOptionBdSet _ANSI_ARGS_((ClientData clientData,
 			Tcl_Interp *interp, Tk_Window tkwin,
-			char *value, char *widgRec, int offset));
+			CONST84 char *value, char *widgRec, int offset));
 extern char *	TableOptionBdGet _ANSI_ARGS_((ClientData clientData,
 			Tk_Window tkwin, char *widgRec, int offset,
 			Tcl_FreeProc **freeProcPtr));
 extern int	TableTagConfigureBd _ANSI_ARGS_((Table *tablePtr,
 			TableTag *tagPtr, char *oldValue, int nullOK));
 extern int	Cmd_OptionSet _ANSI_ARGS_((ClientData clientData,
-					   Tcl_Interp *interp,
-					   Tk_Window unused, char *value,
-					   char *widgRec, int offset));
+			Tcl_Interp *interp,
+			Tk_Window unused, CONST84 char *value,
+			char *widgRec, int offset));
 extern char *	Cmd_OptionGet _ANSI_ARGS_((ClientData clientData,
-					   Tk_Window unused, char *widgRec,
-					   int offset,
-					   Tcl_FreeProc **freeProcPtr));
+			Tk_Window unused, char *widgRec,
+			int offset, Tcl_FreeProc **freeProcPtr));
 
 /*
  * HEADERS IN tkTableCell.c
--- generic/tkTableUtil.c
+++ generic/tkTableUtil.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkTableUtil.c,v 1.2 2002/03/13 20:15:20 hobbs Exp $
+ * RCS: @(#) $Id: tkTableUtil.c,v 1.3 2002/03/21 02:18:27 hobbs Exp $
  */
 
 #include "tkTable.h"
@@ -74,7 +74,7 @@ TableOptionBdSet(clientData, interp, tkwin, value, widgRec, offset)
     ClientData clientData;		/* Type of struct being set. */
     Tcl_Interp *interp;			/* Used for reporting errors. */
     Tk_Window tkwin;			/* Window containing table widget. */
-    char *value;			/* Value of option. */
+    CONST84 char *value;		/* Value of option. */
     char *widgRec;			/* Pointer to record for item. */
     int offset;				/* Offset into item. */
 {
@@ -302,7 +302,7 @@ TableTagConfigureBd(Table *tablePtr, TableTag *tagPtr,
 
 int
 Cmd_OptionSet(ClientData clientData, Tcl_Interp *interp,
-	      Tk_Window unused, char *value, char *widgRec, int offset)
+	Tk_Window unused, CONST84 char *value, char *widgRec, int offset)
 {
   Cmd_Struct *p = (Cmd_Struct *)clientData;
   int mode = Cmd_GetValue(p,value);
